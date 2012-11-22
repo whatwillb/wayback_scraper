ec57c23fd1f95296777588d40f510f84911fb6de
Apprentice <apprentice@dbc05.local>
2012-11-21 21:52:27 -0800

    deleted files manually

--- db/migrate/20121118121500_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :username
-      t.text :email
-      t.integer :user_code
-    end
-  end
-end
--- db/migrate/20121118121900_create_confessions.rb
+++
@@ -1,9 +0,0 @@
-class CreateConfessions < ActiveRecord::Migration
-  def change
-    create_table :confessions do |t|
-      t.text     :body
-      t.integer  :post_id
-    end
-    add_index :confessions, [:post_id], :unique => true
-  end
-end
